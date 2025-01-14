4e537b8e5daa016b6f8a91275e584782be55b1cc
Cassio <cassiojr0108@gmail.com>
2025-01-14 12:43:57 -0300

    feat(UserValidator): create user validation

---
+++ src/users/domain/validators/user.validator.ts
@@ -0,0 +1,74 @@
+import { ClassValidatorFields } from '@/shared/domain/validators/class-validator-fields'
+import { UserProps } from '../entities/user.entity'
+import {
+  IsEmail,
+  IsIn,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator'
+import { UserType } from '../enums/user.type.enum'
+
+export class UserRules {
+  @MaxLength(255)
+  @IsString()
+  @IsNotEmpty()
+  firstName: string
+
+  @MaxLength(255)
+  @IsString()
+  @IsNotEmpty()
+  lastName: string
+
+  @MaxLength(14)
+  @MinLength(11)
+  @IsString()
+  @IsEmail()
+  @IsNotEmpty()
+  @Matches(/^\d{3}\.\d{3}\.\d{3}-\d{2}$|^\d{2}\.\d{3}\.\d{3}\/\d{4}-\d{2}$/, {
+    message: 'document must be a valid CPF or CNPJ',
+  })
+  document: string
+
+  @MaxLength(255)
+  @IsString()
+  @IsEmail()
+  @IsNotEmpty()
+  email: string
+
+  @MaxLength(255)
+  @IsNumber()
+  @IsNotEmpty()
+  balance: number
+
+  @MaxLength(100)
+  @IsString()
+  @IsNotEmpty()
+  password: string
+
+  @MaxLength(100)
+  @IsIn([UserType.COMMON, UserType.LOGIST], {
+    message: 'userType must be a valid value.',
+  })
+  @IsNotEmpty()
+  userType: UserType
+
+  constructor(userProps: UserProps) {
+    Object.assign(this, { ...userProps })
+  }
+}
+
+export class UserValidator extends ClassValidatorFields<UserRules> {
+  validate(data: UserRules): boolean {
+    return super.validate(new UserRules(data ?? ({} as UserProps)))
+  }
+}
+
+export class UserValidatorFactory {
+  static create(): UserValidator {
+    return new UserValidator()
+  }
+}
