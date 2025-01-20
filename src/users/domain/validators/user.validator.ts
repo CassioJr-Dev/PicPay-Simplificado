97fe76588b37a74d980048cdd34f2d8abde0fd64
Cassio <cassiojr0108@gmail.com>
2025-01-20 11:13:18 -0300

    refactor(UserValidator):refactor error

--- src/users/domain/validators/user.validator.ts
+++ src/users/domain/validators/user.validator.ts
@@ -26,7 +26,6 @@ export class UserRules {
   @MaxLength(14)
   @MinLength(11)
   @IsString()
-  @IsEmail()
   @IsNotEmpty()
   @Matches(/^\d{3}\.\d{3}\.\d{3}-\d{2}$|^\d{2}\.\d{3}\.\d{3}\/\d{4}-\d{2}$/, {
     message: 'document must be a valid CPF or CNPJ',
