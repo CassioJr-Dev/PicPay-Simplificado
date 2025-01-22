68678375769d3e79ebf7c356a9a08c9dc328ee8f
Cassio <cassiojr0108@gmail.com>
2025-01-22 16:42:55 -0300

    feat(UserDataBuilder): creation of user faker helper

---
+++ src/users/domain/testing/helpers/user-data-builder.ts
@@ -0,0 +1,27 @@
+import { faker } from '@faker-js/faker'
+import { UserType } from '../../enums/user.type.enum'
+import { UserProps } from '../../entities/user.entity'
+
+type Props = {
+  firstName?: string
+  lastName?: string
+  document?: string
+  email?: string
+  balance?: number
+  password?: string
+  userType?: UserType
+  createdAt?: Date
+}
+
+export function UserDataBuilder(props: Props): UserProps {
+  return {
+    firstName: props.firstName ?? faker.person.firstName(),
+    lastName: props.lastName ?? faker.person.lastName(),
+    document: props.document ?? faker.helpers.replaceSymbols('###.###.###-##'),
+    email: props.email ?? faker.internet.email(),
+    balance: props.balance ?? Number(faker.finance.amount({ min: 0.1 })),
+    password: props.password ?? faker.internet.password(),
+    userType: props.userType ?? UserType.COMMON,
+    createdAt: props.createdAt ?? new Date(),
+  }
+}
