ed77ebd6dbf822c2283b721aafcce2b03a306275
Cassio <cassiojr0108@gmail.com>
2025-01-29 14:46:34 -0300

    feat(UserRepositoryInterface): creating UserRepository contract

---
+++ src/users/domain/repositories/user.repository.ts
@@ -0,0 +1,8 @@
+import { IRepositoryInterface } from '@/shared/domain/repositories/repository-contract'
+import { UserEntity } from '../entities/user.entity'
+
+export interface IUserRepositoryInterface
+  extends IRepositoryInterface<UserEntity> {
+  findByEmail(email: string): Promise<UserEntity>
+  emailExists(email: string): Promise<void>
+}
