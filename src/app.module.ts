83f33fd9b0c11cd5c4a62c74b293af8e914723d4
Cassio <cassiojr0108@gmail.com>
2025-04-27 19:29:55 -0300

    config(): update configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,35 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { EnvConfigModule } from './shared/infrastructure/env-config/env-config.module'
+import { DatabaseModule } from './shared/infrastructure/database/database.module'
+import { UsersModule } from './users/presentation/users.module'
+import { TransactionController } from './transaction/presentation/transaction.controller'
+import { TransactionModule } from './transaction/presentation/transaction.module'
+import { BullModule } from '@nestjs/bull'
+import { SendEmailProvider } from './shared/infrastructure/providers/email-provider/sendEmail.provider'
+import { CqrsModule } from '@nestjs/cqrs'
 
 @Module({
-  imports: [EnvConfigModule],
-  controllers: [AppController],
+  imports: [
+    BullModule.forRoot({
+      redis: 'redis://localhost:6379',
+      defaultJobOptions: {
+        removeOnComplete: 100,
+        removeOnFail: 1000,
+        attempts: 3,
+        backoff: {
+          type: 'exponential',
+          delay: 1000,
+        },
+      },
+    }),
+    CqrsModule,
+    EnvConfigModule,
+    UsersModule,
+    TransactionModule,
+    DatabaseModule,
+  ],
+  controllers: [AppController, TransactionController],
   providers: [AppService],
 })
 export class AppModule {}
