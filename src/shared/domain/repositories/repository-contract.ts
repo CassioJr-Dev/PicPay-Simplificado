7abbcd852e891517512908a3f99894b950e40425
Cassio <cassiojr0108@gmail.com>
2025-04-30 16:40:43 -0300

    refactory(): refactory repository contract

--- src/shared/domain/repositories/repository-contract.ts
+++ src/shared/domain/repositories/repository-contract.ts
@@ -3,7 +3,7 @@ import { Entity } from '../entities/entity'
 export interface IRepositoryInterface<E extends Entity> {
   insert(entity: E): Promise<void | E>
   findById(id: string): Promise<E>
-  findAll(): Promise<E[]>
-  update(entity: E): Promise<void>
+  findAll(userId?: string): Promise<E[]>
+  update(entity: E): Promise<E>
   delete(id: string): Promise<void>
 }
