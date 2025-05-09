1c9f6b6d6778f59cf03992e9692924a1f4a8ba5b
Cassio <cassiojr0108@gmail.com>
2025-05-08 21:33:14 -0300

    Refactory(DataBuilder): refactory transationDataBuilder

--- src/transaction/domain/testing/helpers/transaction-data-builder.ts
+++ src/transaction/domain/testing/helpers/transaction-data-builder.ts
@@ -1,4 +1,4 @@
-import { faker } from '@faker-js/faker'
+import { faker } from '@faker-js/faker/.'
 import { randomUUID } from 'node:crypto'
 import { TransactionProps } from '../../entities/transaction.entity'
 
